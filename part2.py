24d9df4b02e193d06101bfaecd2dad29ac7bd99b
jennie mah <103280932+jjjjennieee@users.noreply.github.com>
2022-05-20 01:43:01 +0000

    i deleted the parenthesis in the first if statement and replaced it with a colon at the end, changed the second if statement into an elif statement and deleted the parenthesis there, added an indent to the print statement underneath it, and added a colon to the else statement. i also added spaces between if and elif and whatever came after it

--- part2.py
+++ part2.py
@@ -13,9 +13,7 @@ num3 = int(input("Enter another number: "))
 
 if num1 == num2 == num3:
   print("All three numbers are equal.")
-
 elif num1 == num2 or num2 == num3 or num1 == num3:
   print("Exactly two of the numbers are equal.")
-
 else:
   print("None of the numbers are equal.")
